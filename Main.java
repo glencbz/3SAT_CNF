e845fdca2694d48a2d6e47c44ec3a56a6e34df08
gleniam <glencbz@gmail.com>
2014-11-18 15:30:43 +0800

    Revert "PPSZ"
    This reverts commit 83602f99121e50cfc4da54c476b039cf1f09101a.

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-	public static void main(String[] args){
-		
-		CNFsolver test = new CNFsolver(CNFparser.parseDimacsCnfFile("src/s8.cnf"));
-		System.out.println(test.getCNF());
-		test.PPSZsearch(test.getCNF());
-	}
-}
