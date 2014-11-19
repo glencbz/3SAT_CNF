77d392cfbe154bb3ccb2747e5b375a2e26aa84c9
gleniam <glencbz@gmail.com>
2014-11-19 22:50:14 +0800

    KNN LA

--- arrayForm/Tester.java
+++ arrayForm/Tester.java
@@ -8,31 +8,12 @@ import java.util.Set;
 public class Tester {
 	public static void main(String[] args){
 		CNFSatInstance test = CNFparser.parseDimacsCnfFile("src/cnf.txt");
-		CNFSatInstance given1 = test.givenVarOccur(test.getClauses(), 4);
+		CNFSatInstance given1 = test.givenVarOccur(-4);
 		
-		for(int[] l : test.getClauses()){
-			for(int i : l){
-				System.out.print(i + " ");
-			}
-			System.out.println("");
-		}
+		System.out.println(test.validClauses());
 		System.out.println("....");
-
-		for(int[] l : test.getOccurrenceMap()){
-			for(int i : l){
-				System.out.print(i + " ");
-			}
-			System.out.println("");
-		}		
-
-		System.out.println(Arrays.toString(test.getOccurrenceNums()));
 		
-		for(int[] l : given1.getClauses()){
-			for(int i : l){
-				System.out.print(i + " ");
-			}
-			System.out.println("");
-		}
+		System.out.println(given1.validClauses());
 		
 		System.out.println("...");
 		
